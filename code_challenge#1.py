a2bef685d7a873745699ac5c7035c3aaf78080a8
SealLover2008 <lloydfritz14@gmail.com>
2026-08-25 21:22:26 +0800

    Activities

---
+++ code_challenge#1.py
@@ -0,0 +1,7 @@
+print("\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t*\t\t*\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t*\t\t\t\t*\n\t\t\t\t\t\t\t\t\t\t\t\t\t*\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t\t\t\t\t*\t\t\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t\t\t\t\t*\t\t\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t\t\t\t       * *\t\t\t\t\t\t\t       * *\n\t\t\t\t\t\t\t\t\t\t\t       * *\t\t\t\t\t\t\t       * *\n\t\t\t\t\t\t\t\t\t\t\t       * *\t\t\t\t\t\t\t       * *\n\t\t\t\t\t\t\t\t\t\t\t       * *\t\t Hi, My Name is Lloyd_Fritz\t\t       * *\n\t\t\t\t\t\t\t\t\t\t\t       * *\t\t From BSIT-1B\t\t\t\t       * *\n\t\t\t\t\t\t\t\t\t\t\t       * *\t\t\t\t\t\t\t       * *\n\t\t\t\t\t\t\t\t\t\t\t       * *\t\t\t\t\t\t\t       * *\n\t\t\t\t\t\t\t\t\t\t\t       * *\t\t\t\t\t\t\t       * *\n\t\t\t\t\t\t\t\t\t\t\t       * *\t\t\t\t\t\t\t       * *\n\t\t\t\t\t\t\t\t\t\t\t       * *\t* \t* \t* \t* \t* \t* \t*      * *\n\t\t\t\t\t\t\t\t\t\t\t       * * \t* \t* \t* \t* \t* \t* \t*      * *") 
+
+
+
+
+
+
