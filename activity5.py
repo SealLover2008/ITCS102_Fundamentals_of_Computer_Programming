3498dcf4de29ffc26d11dc2de1bf9073f0ee8007
SealLover2008 <lloydfritz14@gmail.com>
2026-08-26 11:09:09 +0800

    activity5
    progress

---
+++ activity5.py
@@ -0,0 +1,26 @@
+#Operators Activity 
+
+a = 8
+b = 2
+c = 7
+
+sum = a + b
+subtract = a - b 
+multiply = a * b 
+exponent = a ** b
+floor_division = a // c
+modolo = a % b
+division = a / b
+
+
+
+print("the number", a , "and ", b ,"has the sum of ", sum) 
+print(" has the difference of ", subtract)
+print(" Has the times ", multiply)
+print(" Has the exponent of ", exponent)
+print(" Has the f divion of ", floor_division)
+print(" Has the modulo of ", modolo)
+print(" Has the division of ", division)
+
+ 
+
