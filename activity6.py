2b59f6a4ee273b99fefcfb43ab62549d15127735
SealLover2008 <lloydfritz14@gmail.com>
2026-09-02 12:11:16 +0800

    New lessons/Changes to code challenge2

---
+++ activity6.py
@@ -0,0 +1,36 @@
+name = input( " What is your name? " ) 
+
+print( " \nWell hello", name,"!" )
+
+Date_of_birth = int(input("\nWhen were you born?"))
+
+age = 2026 - Date_of_birth
+
+print("\nSo you are", age ," old ?")
+
+if age >= 18:
+	print(" \nAhhh old enough I see")
+else:
+	print("""
+    ____
+ __/    \\__
+l_ O____O___l   
+""", "\nBawal kapa ditooo boss ;) ", "\n ito kotse laro ka muna")
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
