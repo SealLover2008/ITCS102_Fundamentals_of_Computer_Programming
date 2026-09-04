827a159c595a9f2ffc0a5e29b8590da838755c39
SealLover2008 <lloydfritz14@gmail.com>
2026-09-04 22:46:59 +0800

    New activities

---
+++ activity7.py
@@ -0,0 +1,35 @@
+total = 5
+
+a = eval(input("ENTER ANY NUMBER ====>"))
+total += a
+
+b = eval(input("ANYTHING ELSE? ====>"))
+total -= b
+
+c = eval(input("ANYTHING ELSE? ====>"))
+total *= c
+
+d = eval(input("ANYTHING ELSE? ====>"))
+total -= d
+
+e = eval(input("ANYTHING ELSE? ====>"))
+total += e
+
+f = eval(input("ANYTHING ELSE? ====>"))
+total += f
+
+g = eval(input("ANYTHING ELSE? ====>"))
+total -= g
+
+h = eval(input("ANYTHING ELSE? ====>"))
+total += h
+
+i = eval(input("ANYTHING ELSE? ====>"))
+total -= i
+
+j = eval(input("ANYTHING ELSE? ====>"))
+total += j
+
+print(type(total))
+
+print("Total is", total)
---
+++ activity8.py
@@ -0,0 +1,15 @@
+hobbies = ""
+
+a = input("My Hobbies is ===>")
+hobbies += a + ", "
+
+a = input("HMMMMMM ===>")
+hobbies += a + ", "
+
+a = input("You do say? ===>")
+hobbies += a + ", "
+
+a = input("Anything else? ====>")
+hobbies += a + ", "
+
+print("my hobbies are {", hobbies ,"}")
