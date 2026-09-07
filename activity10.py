5e4eb43e6bc1a8453bb83fae6064516d3039591a
SealLover2008 <lloydfritz14@gmail.com>
2026-09-07 11:15:39 +0800

    Activity 10

---
+++ activity10.py
@@ -0,0 +1,31 @@
+#basic if else 
+
+
+username = "user1"
+password = "araykopo1"
+
+u = input("input USERNAME ====>") 
+p = input("input PASSWORD ====>")
+
+
+if u == username or p == password:
+
+	print(" => Hello ", username, " <= ")
+else:
+	print(" => Wrong username & Password   <= ")
+
+
+#if u == username:
+	print(" => Hello ", username, " <= ")
+#else:
+	print(" => Wrong USERNAME <= ")
+
+
+#if p== password:
+	print(" => Correct PASSWORD <= ")
+#else:
+	print(" => Wrong PASSWORD <= ")
+
+
+  
+
